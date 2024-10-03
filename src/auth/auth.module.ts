3141ede8d011b85009129dd4f37252161bdd6fbf
muimui69 <moises_c2015@outlook.es>
2024-10-03 12:05:54 -0400

    .

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -15,8 +15,12 @@ import { AuthResolver } from './auth.resolver';
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'),
+      // useFactory: (configService: ConfigService) => ({
+      //   secret: configService.get<string>('JWT_SECRET'),
+      //   signOptions: { expiresIn: '1d' },
+      // }),
+      useFactory: () => ({
+        secret: "dominicagaa",
         signOptions: { expiresIn: '1d' },
       }),
     }),
--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { ConfigService } from '@nestjs/config';
+// import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor(configService: ConfigService) {
+    // constructor(configService: ConfigService) {
+    constructor() {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
-            secretOrKey: configService.get<string>('JWT_SECRET'),
+            // secretOrKey: configService.get<string>('JWT_SECRET'),
+            secretOrKey: "dominicagaa",
         });
     }
 
