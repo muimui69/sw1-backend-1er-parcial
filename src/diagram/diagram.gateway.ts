362740d0097c2fea7a31a0137c2d0746210ec83b
muimui69 <moises_c2015@outlook.es>
2024-10-08 12:17:26 -0400

    .

--- src/diagram/diagram.gateway.ts
+++ src/diagram/diagram.gateway.ts
@@ -8,7 +8,7 @@ import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({
   cors: {
-    origin: '*', // Asegúrate de configurar CORS correctamente
+    origin: '*',
   },
 })
 export class DiagramGateway implements OnGatewayConnection, OnGatewayDisconnect {
@@ -17,32 +17,63 @@ export class DiagramGateway implements OnGatewayConnection, OnGatewayDisconnect 
 
   handleConnection(client: Socket) {
     console.log('Cliente conectado: ' + client.id);
-    client.on('shape-add', (data) => {
-      console.log('Servidor recibió shape-add:', data);
-      client.broadcast.emit('shape-add', data);
+
+    client.on('joinRoom', (room: string) => {
+      client.join(room);
+      client.data.room = room;
+      console.log(`Cliente ${client.id} se unió a la sala ${room}`);
     });
 
 
+    client.on('leaveRoom', () => {
+      const room = client.data.room;
+      if (room) {
+        client.leave(room);
+        console.log(`Cliente ${client.id} salió de la sala ${room}`);
+        delete client.data.room;
+      }
+    });
+
+    client.on('shape-add', (data) => {
+      console.log('Servidor recibió shape-add:', data);
+      const room = client.data.room;
+      if (room) {
+        client.to(room).emit('shape-add', data);
+      }
+    });
+
     client.on('shape-move', (data) => {
       console.log('Servidor recibió shape-move:', data);
-      client.broadcast.emit('shape-move', data);
+      const room = client.data.room;
+      if (room) {
+        client.to(room).emit('shape-move', data);
+      }
     });
 
     client.on('shape-remove', (data) => {
       console.log('Servidor recibió shape-remove:', data);
-      client.broadcast.emit('shape-remove', data);
+      const room = client.data.room;
+      if (room) {
+        client.to(room).emit('shape-remove', data);
+      }
     });
 
     client.on('shape-update', (data) => {
       console.log('Servidor recibió shape-update:', data);
-      client.broadcast.emit('shape-update', data);
+      const room = client.data.room;
+      if (room) {
+        client.to(room).emit('shape-update', data);
+      }
     });
-
   }
 
   handleDisconnect(client: Socket) {
     console.log('Cliente desconectado: ' + client.id);
+    const room = client.data.room;
+    if (room) {
+      client.leave(room);
+      console.log(`Cliente ${client.id} salió de la sala ${room} al desconectarse`);
+      delete client.data.room;
+    }
   }
-
-
 }
